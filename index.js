439a0229deea1bb57181d9a1b3c3924ba5f40995
Nayani_Karthik <nayanikarthik961@gmail.com>
2025-04-11 18:00:12 +0530

    commit

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-console.log("Hello, world!");
-console.log("This is a test.");
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('This is a GET request response!\n');
+  } else {
+    res.statusCode = 405;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Only GET requests are supported.\n');
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
